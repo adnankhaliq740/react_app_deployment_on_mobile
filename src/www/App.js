7138a18ac0f133ba22a4a512072f3b4169c491e0
Adnankhaliq <66030199+adnankhaliq740@users.noreply.github.com>
2020-06-03 15:47:03 +0500

    Update App.js

--- src/www/App.js
+++ src/www/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logos from './logos.svg'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          This site was deployed on mobile with github actions
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    count: 'loading...'
+  }
+
+  componentDidMount = async () => {
+    const { count } = await window.fetch(`/api/count`).then(res => res.json())
+    this.setState({ count })
+  }
+
+  increment = async () => {
+    const { count } = await window
+      .fetch(`/api/count/increment`, { method: 'POST' })
+      .then(res => res.json())
+    this.setState({ count })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logos} className="App-logo" alt="logo" />
+          <p>
+            {'Learn '}
+            <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+              React
+            </a>
+            {', '}
+            <a href="https://expressjs.com" target="_blank" rel="noopener noreferrer">
+              Express
+            </a>
+            {', and '}
+            <a href="https://kubernetes.io" target="_blank" rel="noopener noreferrer">
+              Kubernetes
+            </a>
+          </p>
+          <p>
+            Modify <code>src/www/App.js</code> or <code>src/api/index.js</code> to reload UI or API.
+          </p>
+          <p>
+            <code>yarn deploy</code>this site was
+          </p>
+          <hr />
+          <h2>Count: {this.state.count}</h2>
+          <p>
+            Call <code>/api/count/increment</code>
+            <button onClick={this.increment} className="App-button">
+              Go
+            </button>
+          </p>
+        </header>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
